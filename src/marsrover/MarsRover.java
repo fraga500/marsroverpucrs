8fd51a49d3ec1de891564858736b6f875fe346bf
PEDRO FRANCESCHINI DE FRAGA <12106048@pucrs.br>
2018-03-16 18:45:02 -0300

    teste

--- src/marsrover/MarsRover.java
+++ src/marsrover/MarsRover.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package marsrover;
 
-/**
- *
- * @author 12106048
- */
 public class MarsRover {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public int posInicialX, posInicialY, posFinalX, posFinalY = 0;
     
+    
+    public static void main(String[] args) {
+
+    }
+
 }
