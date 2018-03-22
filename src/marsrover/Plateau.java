18bc3ddffd853186a1603d92b66241b156640f41
mthgatti <mthgatti@gmail.com>
2018-03-22 16:58:35 -0300

    menu de interface para adicionar rovers

--- src/marsrover/Plateau.java
+++ src/marsrover/Plateau.java
@@ -1,5 +1,28 @@
 package marsrover;
 
 public class Plateau {
+	
+	private static int x;
+	private static int y;
+	
+	public Plateau(int x, int y) {
+		this.x = x;
+		this.y= y;
+	}
+	
+	public int getMaxX() {
+		return x;
+	}
+	
+	public int getMaxY() {
+		return y;
+	}
+
+	@Override
+	public String toString() {
+		return "Plateau [Tamanho x: " + getMaxX() + ", Tamanho y: " + getMaxY() + "]";
+	}
+	
+	
 
 }
